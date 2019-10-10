2b7235588a6b344096c366716dcfec8f505801d1
Woorin Jang <woorinjang@uchicago.edu>
2019-10-10 17:40:18 -0500

    lab2 work

--- java/src/edu/virginia/engine/display/AnimatedSprite.java
+++ java/src/edu/virginia/engine/display/AnimatedSprite.java
@@ -8,7 +8,7 @@ import java.util.ArrayList;
 
 public class AnimatedSprite extends Sprite {
 
-    private static final int DEFAULT_ANIMATION_SPEED = 0;
+    private static final int DEFAULT_ANIMATION_SPEED = 0; // in ms
     private ArrayList<Animation> animations;
     private String fileName;
     private ArrayList<BufferedImage> frames;
@@ -17,7 +17,7 @@ public class AnimatedSprite extends Sprite {
     private int startFrame;
     private int endFrame;
     private GameClock gameClock;
-    private int animationSpeed;
+    private int animationSpeed; // check against gameClock
 
     public AnimatedSprite(String id, String imageFileName, Point position) {
         super(id, imageFileName);
@@ -26,9 +26,29 @@ public class AnimatedSprite extends Sprite {
         this.animationSpeed = DEFAULT_ANIMATION_SPEED;
     }
 
+    public Animation getAnimation(String id) {
+    }
+
+    public void setAnimationSpeed(int newAnimationSpeed) {
+        this.animationSpeed = newAnimationSpeed;
+    }
+
+    public void setAnimations(ArrayList<Animation> newAnimations) {
+        this.animations = newAnimations;
+    }
+
     public void initGameClock() {
         if (this.gameClock == null) {
             this.gameClock = new GameClock();
         }
+    }
+
+    @Override
+    public void draw(Graphics g) {
+        if (playing && animationSpeed <= gameClock.getElapsedTime()) {
+            currentFrame++;
+            gameClock.resetGameClock();
+        }
+        super.draw(g);
     }
 }
